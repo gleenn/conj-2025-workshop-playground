6b98d03e8635635462570d84dae8e3173d9509e3
Ethan Miller <ethanzanemiller@gmail.com>
2025-11-11 07:35:34 -0500

    Clean up

--- notebooks/test.clj
+++ notebooks/test.clj
@@ -1,11 +1,5 @@
 (ns test
   (:require [tablecloth.column.api :as tcc]))
-;;=> nil
 
 (tcc/+ [1 2 3] [4 5 6])
-;;=> #tech.v3.dataset.column<int64>[3]
-;;   null
-;;   [5, 7, 9]
-
-![desired output](test-output.png)
 
